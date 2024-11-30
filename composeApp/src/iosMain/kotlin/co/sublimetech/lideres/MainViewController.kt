f9049ee2626fc243fdebd310d02cfff09f834701
felipecoro <f.coronado838@gmail.com>
2024-11-30 14:27:03 -0500

    initial commit

---
+++ composeApp/src/iosMain/kotlin/co/sublimetech/lideres/MainViewController.kt
@@ -0,0 +1,6 @@
+package co.sublimetech.lideres
+
+import androidx.compose.ui.window.ComposeUIViewController
+import co.sublimetech.lideres.app.App
+
+fun MainViewController() = ComposeUIViewController { App() }
